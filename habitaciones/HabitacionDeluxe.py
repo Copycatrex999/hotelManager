46ce13514792db065290d965c7cd5fc454758cb6
copycatrex999 <copycatrex@outlook.coom>
2026-03-14 17:19:59 -0700

    Agrega el patrón Singleton para la base de datos

--- habitaciones/HabitacionDeluxe.py
+++ habitaciones/HabitacionDeluxe.py
@@ -2,8 +2,14 @@ from habitaciones.Habitacion import Habitacion
 
 
 class HabitacionDeluxe(Habitacion):
-    def __init__(self, numero: str, precio_noche: float = 80.0, estado: str = "disponible",
-                 huesped: str = "", noches: int = 0) -> None:
+    def __init__(
+        self,
+        numero: str,
+        precio_noche: float = 80.0,
+        estado: str = "disponible",
+        huesped: str = "",
+        noches: int = 0,
+    ) -> None:
         descripcion: str = "Cama King size, balcón con vista y minibar (Cargo extra de $15)."
         super().__init__(numero, "Deluxe", precio_noche, descripcion, estado, huesped, noches)
 
